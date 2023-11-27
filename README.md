ddcc3392248c59d43073864deea26a2b746fd5ff
JOHNNIENYC <JACKSMITHER71@GMAIL.COM>
2023-11-26 19:54:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+# Disaster Response Pipeline Project
+
+### Instructions:
+1. Run the following commands in the project's root directory to set up your database and model.
+
+    - To run ETL pipeline that cleans data and stores in database
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+    - To run ML pipeline that trains classifier and saves
+        `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+
+2. Go to `app` directory: `cd app`
+
+3. Run your web app: `python run.py`
+
+4. Click the `PREVIEW` button to open the homepage
+=======
+# Udacity--Disaster-Response-Project
+>>>>>>> 5026130a9d45dfd939783d289492f35909f0498c
---
+++ models/train_classifier.py
@@ -0,0 +1,137 @@
+import re
+import os
+import pickle
+from sqlalchemy import create_engine
+import pandas as pd
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+import nltk
+import sys
+from sklearn.metrics import classification_report
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table("DisasterResponse_Table", engine)
+    X = df.message
+    y = df.iloc[:, 4:]
+    category_names = y.columns
+    return X, y, category_names
+
+def tokenize(text):
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+    
+def build_model():
+    
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+            ('starting_verb', StartingVerbExtractor())
+        ])),
+
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+        
+    parameters = {
+        'clf__estimator__n_estimators': [5, 10, 20],
+        'clf__estimator__max_depth': [None, 10],
+        
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
+
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    
+    y_pred = model.predict(X_test)
+    
+    result_report = classification_report(Y_test, y_pred, target_names=category_names)
+
+    print("classification report:\n", result_report)
+
+def save_model(model, model_filepath):
+    
+    file = open(model_filepath, 'wb')
+    try:
+        pickle.dump(model, file)
+    finally:
+        file.close()
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
